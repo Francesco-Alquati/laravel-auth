bd626156e2a9165f59cc648a77bd6caff4e4d45c
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-10-08 00:12:34 +0200

    [UPD] Added some style to the table

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,34 +1,35 @@
 @extends('layouts.dashboard')
 
 @section('main-content')
-    <div class="container-fluid">
+    <div class="container">
         <div class="row">
             <div class="col-12">
-                <div class="d-flex  align-items-center justify-content-between p-3">
-                    <h2>Progetti</h2>
-                    <a href="{{ route('admin.projects.create') }}" class="btn btn-primary btn-sm">Aggiungi Progetto</a>
+                <div class="d-flex align-items-center justify-content-between p-2">
+                    <h2 class="text-uppercase">Progetti</h2>
+                    <a href="{{ route('admin.projects.create') }}" class="btn btn-primary btn-sm">+ Add</a>
                 </div>
             </div>
             <div class="col-12">
-                <table class="table table-success table-striped">
+                <table class="table table-striped border border-black">
                     <thead>
                         <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Slug</th>
-                            <th>Strumenti</th>
+                            <th class="border border-left border-black text-center">ID</th>
+                            <th class="border border-left border-black">Name</th>
+                            <th class="border border-left border-black">Slug</th>
+                            <th class="border border-left border-black text-center">Strumenti</th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach ($projects as $project)
                         <tr>
-                            <td>{{ $project->id }}</td>
-                            <td>{{ $project->name }}</td>
-                            <td>{{ $project->slug }}</td>
-                            <td>
-                                <div class="d-flex">
-                                    <a href="{{ route('admin.projects.show', ['project' => $project->id]) }}" class="btn btn-sm btn-primary"><i class="fa-solid fa-eye"></i></a>
+                            <td class="border border-left border-black text-center">{{ $project->id }}</td>
+                            <td class="border border-left border-black">{{ $project->name }}</td>
+                            <td class="border border-left border-black">{{ $project->slug }}</td>
+                            <td class="border border-left border-black">
+                                <div class="d-flex align-items-center justify-content-center">
+                                    <a href="{{ route('admin.projects.show', ['project' => $project->id]) }}" class="btn btn-sm btn-info"><i class="fa-solid fa-eye"></i></a>
                                     <a href="{{ route('admin.projects.edit', ['project' => $project->id]) }}" class="btn btn-sm btn-warning ms-1"><i class="fa-solid fa-pen-to-square"></i></a>
+                                    <a href="" class="btn btn-sm btn-danger ms-1"><i class="fa-solid fa-trash" style="color: #000000;"></i></a>
                                 </div>
                             </td>
                         </tr>
