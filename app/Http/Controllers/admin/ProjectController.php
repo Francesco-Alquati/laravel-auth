3887df5faf957d4dc13d20ff145892132ccbab5a
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-10-09 20:36:58 +0200

    [UPD] Updated destroy method

--- app/Http/Controllers/admin/ProjectController.php
+++ app/Http/Controllers/admin/ProjectController.php
@@ -117,6 +117,11 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
+
+        if($project->image != null){
+            Storage::disk('public')->delete($project->image);
+        }
+        
         $project->delete();
 
         return redirect()->route('admin.projects.index');
